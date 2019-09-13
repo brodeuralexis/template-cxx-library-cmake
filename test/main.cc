48a3fbbf6d45c0f20624dc9c4df2e3b6f6daca4e
Alexis Brodeur <brodeuralexis@gmail.com>
2019-09-13 09:16:31 -0400

    Support googletest as a testing framework

--- test/main.cc
+++
@@ -1,2 +0,0 @@
-#define BOOST_TEST_MODULE "my_library"
-#include <boost/test/included/unit_test.hpp>
--- test/my_library/my_library_test.cc
+++ test/my_library/my_library_test.cc
@@ -1,12 +1,8 @@
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
 #include <my_library/my_library.hh>
 
-BOOST_AUTO_TEST_SUITE(MyLibraryTest)
-
-BOOST_AUTO_TEST_CASE(TestIfGreetReturnsAGreeting)
+TEST(MyLibraryTest, TestIfGreetReturnsAGreeting)
 {
-    BOOST_CHECK_EQUAL(my_library::greet(), "Hello from my_library::greet()!");
+    EXPECT_EQ(my_library::greet(), "Hello from my_library::greet()");
 }
-
-BOOST_AUTO_TEST_SUITE_END()
